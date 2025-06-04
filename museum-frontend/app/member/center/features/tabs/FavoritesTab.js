5d135bfaa28b3fbca2a356cb7ae13fa47ca7c67a
Xie <fantasy889921@gmail.com>
2025-06-05 03:25:33 +0800

    feat: 更新outline

--- museum-frontend/app/member/center/features/tabs/FavoritesTab.js
+++ museum-frontend/app/member/center/features/tabs/FavoritesTab.js
@@ -175,7 +175,7 @@ export default function FavoritesTab() {
                     <td>
                       <div className={styles.actionGroup}>
                         <button
-                          className="btn btn-dark btn-sm"
+                          className="btn btn-outline-primary btn-sm"
                           onClick={() =>
                             window.open(
                               `/products/details/${item.id}`,
@@ -186,7 +186,7 @@ export default function FavoritesTab() {
                           <FaEye />
                         </button>
                         <button
-                          className="btn btn-danger btn-sm"
+                          className="btn btn-outline-danger btn-sm"
                           onClick={() => handleRemove(item.id)}
                         >
                           <FaTrashAlt />
